9f4b50ac4e1e31e536a915cd57cbd54eb46fd1c5
Jérémy Huet <jeremy.huet@gmail.com>
2012-10-18 21:05:42 +0200

    Code refactoring with original UserProvider.

--- src/ScnSocialAuthDoctrineMongoODM/Document/UserProvider.php
+++ src/ScnSocialAuthDoctrineMongoODM/Document/UserProvider.php
@@ -2,77 +2,20 @@
 namespace ScnSocialAuthDoctrineMongoODM\Document;
 
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
-use ScnSocialAuth\Entity\UserProviderInterface;
+use ScnSocialAuth\Entity\UserProvider as BaseUserProvider;
 
 /** @ODM\Document(collection="user_provider") */
-class UserProvider implements UserProviderInterface
+class UserProvider extends BaseUserProvider
 {
     /** @ODM\Id */
     private $id;
 
     /** @ODM\String */
-    private $userId;
+    protected $userId;
 
     /** @ODM\String */
     protected $providerId;
 
     /** @ODM\String */
     protected $provider;
-
-    /**
-     * @return the $userId
-     */
-    public function getUserId()
-    {
-        return $this->userId;
-    }
-
-    /**
-     * @param  integer      $userId
-     * @return UserProvider
-     */
-    public function setUserId($userId)
-    {
-        $this->userId = $userId;
-
-        return $this;
-    }
-
-    /**
-     * @return the $providerId
-     */
-    public function getProviderId()
-    {
-        return $this->providerId;
-    }
-
-    /**
-     * @param  integer      $providerId
-     * @return UserProvider
-     */
-    public function setProviderId($providerId)
-    {
-        $this->providerId = $providerId;
-
-        return $this;
-    }
-
-    /**
-     * @return the $provider
-     */
-    public function getProvider()
-    {
-        return $this->provider;
-    }
-
-    /**
-     * @param  string       $provider
-     * @return UserProvider
-     */
-    public function setProvider($provider)
-    {
-        $this->provider = $provider;
-
-        return $this;
-    }
 }
