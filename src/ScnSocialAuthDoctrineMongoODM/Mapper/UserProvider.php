51648b76d0cc31103d1cc095e19fe5155419e3c6
Jérémy Huet <jeremy.huet@gmail.com>
2012-12-07 17:27:04 +0100

    linkUserToProvider method.

--- src/ScnSocialAuthDoctrineMongoODM/Mapper/UserProvider.php
+++ src/ScnSocialAuthDoctrineMongoODM/Mapper/UserProvider.php
@@ -2,29 +2,41 @@
 namespace ScnSocialAuthDoctrineMongoODM\Mapper;
 
 use Doctrine\ODM\MongoDB\DocumentManager;
-use ZfcBase\Mapper\AbstractDbMapper;
+use Hybrid_User_Profile;
+use ScnSocialAuth\Mapper\Exception;
 use ScnSocialAuth\Mapper\UserProviderInterface;
 use ScnSocialAuthDoctrineMongoODM\Options\ModuleOptions;
 use Zend\Stdlib\Hydrator\HydratorInterface;
+use ZfcBase\Mapper\AbstractDbMapper;
+use ZfcUser\Entity\UserInterface;
 
 class UserProvider extends AbstractDbMapper implements UserProviderInterface
 {
     /**
-     * @var \Doctrine\ODM\MongoDB\DocumentManager;
+     * @var DocumentManager;
      */
     protected $dm;
 
     /**
-     * @var \ScnSocialAuthDoctrineMongoODM\Options\ModuleOptions
+     * @var ModuleOptions
      */
     protected $options;
 
+    /**
+     * @param   DocumentManager   $dm
+     * @param   ModuleOptions     $options
+     */
     public function __construct(DocumentManager $dm, ModuleOptions $options)
     {
         $this->dm      = $dm;
         $this->options = $options;
     }
 
+    /**
+     * @param   string    $providerId
+     * @param   string    $provider
+     * @return  UserInterface
+     */
     public function findUserByProviderId($providerId, $provider)
     {
         $dr = $this->dm->getRepository($this->options->getUserProviderEntityClass());
@@ -32,16 +44,57 @@ class UserProvider extends AbstractDbMapper implements UserProviderInterface
         return $document;
     }
 
+    /**
+     * @param   UserInterface       $user
+     * @param   Hybrid_User_Profile $hybridUserProfile
+     * @param   string              $provider
+     * @param   array               $accessToken
+     */
+    public function linkUserToProvider(UserInterface $user, Hybrid_User_Profile $hybridUserProfile, $provider, array $accessToken = null)
+    {
+        $userProvider = $this->findUserByProviderId($hybridUserProfile->identifier, $provider);
+
+        if (false != $userProvider) {
+            if ($user->getId() == $userProvider->getUserId()) {
+                // already linked
+                return;
+            }
+            throw new Exception\RuntimeException('This ' . ucfirst($provider) . ' profile is already linked to another user.');
+        }
+
+        $userProvider = clone($this->getEntityPrototype());
+        $userProvider->setUserId($user->getId())
+                     ->setProviderId($hybridUserProfile->identifier)
+                     ->setProvider($provider);
+        $this->insert($userProvider);
+    }
+
+    /**
+     * @param   UserInterface     $document
+     * @param   string            $tableName
+     * @param   HydratorInterface $hydrator
+     * @return  UserInterface
+     */
     public function insert($document, $tableName = null, HydratorInterface $hydrator = null)
     {
         return $this->persist($document);
     }
 
+    /**
+     * @param   UserInterface       $document
+     * @param   string              $tableName
+     * @param   HydratorInterface   $hydrator
+     * @return  UserInterface
+     */
     public function update($document, $where = null, $tableName = null, HydratorInterface $hydrator = null)
     {
         return $this->persist($document);
     }
 
+    /**
+     * @param   UserInterface   $document
+     * @return  UserInterface
+     */
     protected function persist($document)
     {
         $this->dm->persist($document);
