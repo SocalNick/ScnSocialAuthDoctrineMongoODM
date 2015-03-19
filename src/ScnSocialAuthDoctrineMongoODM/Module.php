52176ccb076ba02b451574a6f664327311eb3f8b
Fábio Carneiro <fhcs@live.com>
2015-03-19 10:03:20 -0300

    make the modulemanager dependency explicit

--- src/ScnSocialAuthDoctrineMongoODM/Module.php
+++ src/ScnSocialAuthDoctrineMongoODM/Module.php
@@ -2,7 +2,9 @@
 
 namespace ScnSocialAuthDoctrineMongoODM;
 
-class Module
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements ConfigProviderInterface
 {
     public function onBootstrap($e)
     {
